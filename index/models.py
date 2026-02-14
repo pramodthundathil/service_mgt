9e683f09d79a7a416940f8e6f96f9380ab4225db
Pramod Gopinath <pramodgopinath@Pramods-MacBook-Air.local>
2026-02-14 19:38:15 +0530

    new

--- index/models.py
+++ index/models.py
@@ -208,7 +208,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         regex=r'^\+?1?\d{9,15}$',
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
     )
-    phone_number = models.IntegerField(validators=[phone_regex])
+    phone_number = models.CharField(max_length=15)
+
     
     # Role and service center
     role = models.CharField(max_length=20, choices=USER_ROLES)
